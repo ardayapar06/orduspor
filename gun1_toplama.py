d9bff6097bb9ecb8cdfc3259f908cdb6cd48e849
ardayapar <ardayapar52@gmail.com>
2026-04-01 19:31:46 +0300

    gün 1 toplama

---
+++ gun1_toplama.py
@@ -0,0 +1,15 @@
+def toplama(a, b):
+    sonuc = a + b
+    return sonuc
+
+
+def main():
+    sayi1 = 50
+    sayi2 = 2
+
+    print("Toplama işlemi sonucu:")
+    print(toplama(sayi1, sayi2))
+
+
+if __name__ == "__main__":
+    main()
