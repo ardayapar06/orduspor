5f74859ead86ce3466fd776d1396d9f3c2855f17
ardayapar <ardayapar52@gmail.com>
2026-04-03 21:40:36 +0300

    gün 3 carpma islemi

---
+++ gun3_carpma.py
@@ -0,0 +1,15 @@
+def carpma(a, b):
+    sonuc = a * b
+    return sonuc
+
+
+def main():
+    sayi1 = 7
+    sayi2 = 6
+
+    print("Çarpma işlemi sonucu:")
+    print(carpma(sayi1, sayi2))
+
+
+if __name__ == "__main__":
+    main()
