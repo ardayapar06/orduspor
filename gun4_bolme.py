e298fa217fc27b673545dd756078849d39dde1a4
ardayapar <ardayapar52@gmail.com>
2026-04-04 16:34:59 +0300

    gün 4 bölme işlemi

---
+++ gun4_bolme.py
@@ -0,0 +1,15 @@
+def bolme(a, b):
+    sonuc = a / b
+    return sonuc
+
+
+def main():
+    sayi1 = 20
+    sayi2 = 4
+
+    print("Bölme işlemi sonucu:")
+    print(bolme(sayi1, sayi2))
+
+
+if __name__ == "__main__":
+    main()
