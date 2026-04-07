be3522956db1fe73c4c3c8b0423f4280a506173a
ardayapar <ardayapar52@gmail.com>
2026-04-07 14:37:10 +0300

    gün 7 üs alma işlemi - final

---
+++ gun7_us.py
@@ -0,0 +1,15 @@
+def us_alma(a, b):
+    sonuc = a ** b
+    return sonuc
+
+
+def main():
+    sayi1 = 2
+    sayi2 = 5
+
+    print("Üs alma işlemi sonucu:")
+    print(us_alma(sayi1, sayi2))
+
+
+if __name__ == "__main__":
+    main()
